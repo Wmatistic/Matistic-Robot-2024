9b13b188386c584d1fe311b4c01226db43193949
Wmatistic <52674478+Wmatistic@users.noreply.github.com>
2024-10-19 01:22:58 -0500

    removed old roadrunner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/Test.java
@@ -1,8 +1,18 @@
 package org.firstinspires.ftc.teamcode.opmodes.auto;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 @Autonomous
-public class Test {
+public class Test extends OpMode {
 
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
 }
